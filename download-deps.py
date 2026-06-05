0bfb609c76db002ea2434e3a0ad56e51f01fa546
oki333ed <kokosik3215@gmail.com>
2026-06-05 05:36:36 +0300

    idk

--- download-deps.py
+++ download-deps.py
@@ -201,7 +201,7 @@ def download_and_unzip_file(url, filename):
 
 def _check_python_version():
     major_ver = sys.version_info[0]
-    if major_ver > 2:
+    if major_ver > 3:
         print(("The python version is %d.%d. But python 2.x is required. (Version 2.7 is well tested)\n"
                "Download it here: https://www.python.org/" % (major_ver, sys.version_info[1])))
         return False
